48372a537a0c2677520b2844996dc0d98874e44e
edstarck <edstarck@users.noreply.github.com>
2023-06-11 12:56:28 +0300

    add config for router

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,6 @@
-<template></template>
+<template>
+  <router-link :to="{ name: 'notes' }">notes</router-link> |
+  <router-link :to="{ name: 'stats' }">stats</router-link>
+
+  <router-view></router-view>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,25 +1,5 @@
-import "./assets/main.css";
-
 import { createApp } from "vue";
-import { createRouter, createWebHashHistory } from "vue-router";
 import App from "./App.vue";
-import NotesView from "@/views/NotesView.vue";
-import StatsView from "@/views/StatsView.vue";
-
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes: [
-    {
-      path: "/",
-      name: "notes",
-      component: NotesView,
-    },
-    {
-      path: "/stats",
-      name: "stats",
-      component: StatsView,
-    },
-  ],
-});
+import router from "@/router";
 
 createApp(App).use(router).mount("#app");
