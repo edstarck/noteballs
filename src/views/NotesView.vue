e85daf116ac76bd0a909c6670b244a578f0ecb3f
edstarck <edstarck@users.noreply.github.com>
2023-08-12 22:16:33 +0300

    implement focus to textarea in child component

--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,8 +1,12 @@
 <script setup>
+/*
+  imports
+*/
 import {ref} from 'vue';
-import Note from '@/components/Notes/Note.vue';
+import OneNote from '@/components/Notes/Note.vue';
 import {useStoreNotes} from '@/stores/storeNotes';
 import {useMouse} from '../use/mouse';
+import AddEditNote from '@/components/Notes/AddEditNote.vue';
 
 // store
 const storeNotes = useStoreNotes();
@@ -10,8 +14,9 @@ const storeNotes = useStoreNotes();
 // composable
 const {x, y} = useMouse();
 
+//state
 const newNote = ref('');
-const newNoteRef = ref(null);
+const addEditNoteRef = ref(null);
 
 /*
   add delete notes
@@ -20,7 +25,7 @@ const newNoteRef = ref(null);
 const addNote = () => {
   storeNotes.add(newNote.value);
   newNote.value = '';
-  newNoteRef.value.focus();
+  addEditNoteRef.value.focusTextarea();
 };
 </script>
 
@@ -28,31 +33,21 @@ const addNote = () => {
   <p class="mb-4">Mouse position is at: {{ x }}, {{ y }}</p>
 
   <div class="notes">
-    <div class="card has-background-success-dark p-4 mb-5">
-      <div class="field">
-        <div class="control">
-          <textarea
-            v-model="newNote"
-            class="textarea"
-            placeholder="Add new note"
-            ref="newNoteRef"
-          ></textarea>
-        </div>
-      </div>
-
-      <div class="field is-grouped is-grouped-right">
-        <div class="control">
-          <button
-            @click="addNote"
-            :disabled="!newNote"
-            class="button is-link has-background-success"
-          >
-            Add New Note
-          </button>
-        </div>
-      </div>
-    </div>
-
-    <Note v-for="note in storeNotes.notes" :key="note.id" :note="note" />
+    <add-edit-note
+      :modelValue="newNote"
+      @update:modelValue="(newValue) => (newNote = newValue)"
+      ref="addEditNoteRef"
+    >
+      <template #buttons>
+        <button
+          @click="addNote"
+          :disabled="!newNote"
+          class="button is-link has-background-success"
+        >
+          Add New Note
+        </button>
+      </template>
+    </add-edit-note>
+    <one-note v-for="note in storeNotes.notes" :key="note.id" :note="note" />
   </div>
 </template>
